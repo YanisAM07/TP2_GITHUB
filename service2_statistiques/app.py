dd4eced668581f49315c2cfa5cb4e3f9ab204b7c
c_gonetpetit <gonetpetitclement@gmail.com>
2026-06-08 17:00:57 +0200

    realisation de la feature Route Post stats  describe

--- service2_statistiques/app.py
+++ service2_statistiques/app.py
@@ -13,5 +13,26 @@ def validate_data(data, key='data'):
         raise ValueError("'data' doit être une liste d'au moins 2 valeurs")     
     return np.array(values, dtype=float) 
 
+@app.route("/stats/describe", methods=["POST"])
+def describe():
+    data = request.get_json()
+    try:
+        values = validate_data(data)
+        result = {
+            "n": int(len(values)),
+            "moyenne": round(float(np.mean(values)), 4),
+            "medianne": round(float(np.median(values)), 4),
+            "ecart_type": round(float(np.std(values, ddof=1)), 4),
+            "variance": round(float(np.var(values, ddof=1)), 4),
+            "minimum": round(float(np.min(values)), 4),
+            "maximum": round(float(np.max(values)), 4),
+            'q1' : round(float(np.percentile(values, 25)), 4),
+            'q3' : round(float(np.percentile(values, 75)), 4),
+            'etendue': round(float(np.ptp(values)), 4)
+        }
+        return jsonify({"operation": "description", "resultat": result})
+    except (ValueError, TypeError) as e:
+        return jsonify({"erreur": str(e)}), 400
+
 if __name__ == '__main__': 
     app.run(debug=True, port=5002) 

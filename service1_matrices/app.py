da9cbba84678e967b8c06fd1ee5bf0bb86c9336e
Nathan-Olv <nathan.oliveira@etu.u-pec.fr>
2026-06-09 16:04:49 +0200

    Ajout de la route et de la fonction pour le déterminant

--- service1_matrices/app.py
+++ service1_matrices/app.py
@@ -60,5 +60,21 @@ def transpose_matrix():
     except (ValueError, TypeError) as e: 
         return jsonify({'erreur': str(e)}), 400
 
+@app.route('/matrices/determinant', methods=['POST'])
+def determinant_matrix(): 
+
+    data = request.get_json()
+    try:
+        A = parse_matrix(data, 'A')
+
+        if A.shape[0] != A.shape[1]: 
+            return jsonify({'erreur': 'La matrice doit etre carree'}), 400
+        
+        det = np.linalg.det(A)
+        return jsonify({'operation': 'determinant', 'resultat': round(det, 6)})
+    
+    except (ValueError, TypeError) as e:
+        return jsonify({'erreur': str(e)}), 400 
+
 if __name__ == '__main__': 
     app.run(debug=True, port=5001) 

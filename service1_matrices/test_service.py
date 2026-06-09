a25cdba1a479c714c4945e87c86ace5aaf416350
Nathan-Olv <nathan.oliveira@etu.u-pec.fr>
2026-06-09 15:56:44 +0200

    Test de la transposé

--- service1_matrices/test_service.py
+++ service1_matrices/test_service.py
@@ -24,3 +24,13 @@ print(response.json())
 print(f"L'opération utilisée est {response.json()["operation"]}.")
 print(f"Le résultat du calcul A est {response.json()["resultat"][0]}.")
 print(f"Le résultat du calcul B est {response.json()["resultat"][1]}.")
+
+url = "http://localhost:5001/matrices/transpose"
+
+response = requests.post(url, json=data)
+
+print("\nTranspose:")
+print(response.json())
+print(f"L'opération utilisée est {response.json()["operation"]}.")
+print(f"Le résultat du calcul A est {response.json()["resultat"][0]}.")
+print(f"Le résultat du calcul B est {response.json()["resultat"][1]}.")
